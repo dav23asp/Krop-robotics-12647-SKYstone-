76e52d4aef1d501a7e770be0fd9a45e1d7be442a
dav23asp <57775067+dav23asp@users.noreply.github.com>
2019-11-14 20:41:34 -0500

    Update SkystoneTeleOp.java

--- SkystoneTeleOp.java
+++ SkystoneTeleOp.java
@@ -1,1 +1,41 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class SkyStoneTeleOp extends SkyStoneLinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        while(!(isStarted()  || isStopRequested())) {
+
+            idle();
+
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // START ROBOT-CENTRIC DRIVETRAIN
+
+            float lefty = -gamepad1.left_stick_y;
+            float leftx = gamepad1.left_stick_x;
+            float rightx = gamepad1.right_stick_x;
+
+            flpower = lefty + leftx + rightx;
+            blpower = lefty - leftx + rightx;
+            frpower = lefty - leftx - rightx;
+            brpower = lefty + leftx - rightx;
+
+            frontLeft.setPower(flpower);
+            backLeft.setPower(blpower);
+            frontRight.setPower(frpower);
+            backRight.setPower(brpower);
+
+            // END ROBOT-CENTRIC DRIVETRAIN
+}
